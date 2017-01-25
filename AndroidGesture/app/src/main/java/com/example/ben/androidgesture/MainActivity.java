851437c0a5f03c73528b35d5486f0742aaec2456
Sheldon Downie <sdownie@live.ca>
2017-01-25 13:11:06 -0500

    I need .idea for some reason DO NOT MERGE THIS INTO MASTER

--- AndroidGesture/app/src/main/java/com/example/ben/androidgesture/MainActivity.java
+++ AndroidGesture/app/src/main/java/com/example/ben/androidgesture/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.ben.androidgesture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+    }
 }
