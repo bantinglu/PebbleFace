e30ede65bb7c5219f8daeeccb03acd98b95c9b0f
Banting Lu <banting.lu.0514@gmail.com>
2017-01-28 01:20:44 -0600

    first commit for branch

--- AndroidGesture/app/src/main/java/com/example/ben/androidgesture/PebbleReceiver.java
+++ AndroidGesture/app/src/main/java/com/example/ben/androidgesture/PebbleReceiver.java
@@ -28,8 +28,23 @@ public class PebbleReceiver extends Activity {
     private static final int PP_KEY_Y   = 2;
     private static final int PP_KEY_Z   = 3;
 
+    @SuppressWarnings("unused")
+    private static final int PP_CMD_INVALID = 0;
+    private static final int PP_CMD_VECTOR  = 1;
+
+    private static final int VECTOR_INDEX_X  = 0;
+    private static final int VECTOR_INDEX_Y  = 1;
+    private static final int VECTOR_INDEX_Z  = 2;
+
+    private static int vector[] = new int[3];
+
     private PebbleKit.PebbleDataReceiver dataReceiver;
     private static final UUID PEBBLE_UUID = UUID.fromString("cf30b8b5-2cee-4f3a-bec8-234523a3ffe4");
+
+    private void updateScreen()
+    {
+
+    }
 
     @Override
     public void onCreate(Bundle savedInstance)
@@ -37,24 +52,36 @@ public class PebbleReceiver extends Activity {
         super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
 
-        /*
-        PebbleDictionary dict = new PebbleDictionary();
-        final int AppKeyContactName = 0;
-        final int AppKeyAge = 1;
-
-        final String contactName = "sheldon";
-        final int age = 12;
-
-        dict.addString(AppKeyContactName, contactName);
-        dict.addInt32(AppKeyAge, age);
-        PebbleKit.sendDataToPebble(getApplicationContext(), PEBBLE_UUID, dict);*/
-
         dataReceiver = new PebbleKit.PebbleDataReceiver(PEBBLE_UUID)
         {
             @Override
-            public void receiveData(final Context context, final int transactionId, final PebbleDictionary dict)
-            {
+            public void receiveData(final Context context, final int transactionId, final PebbleDictionary dict) {
                 PebbleKit.sendAckToPebble(context, transactionId);
+
+                final Long cmdValue = dict.getInteger(PP_KEY_CMD);
+                if (cmdValue == null) {
+                    return;
+                }
+
+                if (cmdValue.intValue() == PP_CMD_VECTOR) {
+
+                    // Capture the received vector.
+                    final Long xValue = dict.getInteger(PP_KEY_X);
+                    if (xValue != null) {
+                        vector[VECTOR_INDEX_X] = xValue.intValue();
+                    }
+
+                    final Long yValue = dict.getInteger(PP_KEY_Y);
+                    if (yValue != null) {
+                        vector[VECTOR_INDEX_Y] = yValue.intValue();
+                    }
+
+                    final Long zValue = dict.getInteger(PP_KEY_Z);
+                    if (zValue != null) {
+                        vector[VECTOR_INDEX_Z] = zValue.intValue();
+                    }
+
+                }
             }
         };
     }
@@ -67,6 +94,7 @@ public class PebbleReceiver extends Activity {
         PebbleKit.startAppOnPebble(getApplicationContext(), PEBBLE_UUID);
 
         PebbleKit.registerReceivedDataHandler(getApplicationContext(), dataReceiver);
+
     }
     @Override
     public void onPause() {
