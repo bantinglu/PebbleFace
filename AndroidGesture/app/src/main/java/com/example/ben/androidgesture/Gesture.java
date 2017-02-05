f832d0ec23f112a1c2a3e28b198c9f1cdca6f48f
Sheldon Downie <sdownie@live.ca>
2017-02-05 12:53:27 -0500

    Added GestureUtil class and output to text file for testing

---
+++ AndroidGesture/app/src/main/java/com/example/ben/androidgesture/Gesture.java
@@ -0,0 +1,20 @@
+package com.example.ben.androidgesture;
+
+
+import java.lang.Enum.*;
+/**
+ * Created by Sheldon on 2/1/2017.
+ */
+
+public final class Gesture {
+
+    Direction direction;
+
+    public Gesture(Direction type){
+        this.direction = direction;
+    }
+
+    public Direction getDir(){
+        return direction;
+    }
+}
---
+++ AndroidGesture/app/src/main/java/com/example/ben/androidgesture/GestureUtil.java
@@ -0,0 +1,22 @@
+package com.example.ben.androidgesture;
+import java.util.*;
+
+/**
+ * Created by Sheldon on 1/31/2017.
+ */
+
+public final class GestureUtil {
+
+
+    public static Gesture recognizeGesture(final List data){
+        // somehow recognize gesture
+        // return whatever direction was detected
+        Gesture gesture = new Gesture(Direction.UP);
+        return gesture;
+    }
+
+    public static void load(){
+        // loads new test data?
+    }
+
+}
